b251f4c6f639f7507f02df376728cd21440e08f5
helenyu <rinmuses@gmail.com>
2023-08-09 10:49:05 -0700

    Add sidebar.

---
+++ src/components/sidebar/SideBar.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import styles from './SideBar.module.css'; // Import css modules stylesheet as styles
+
+
+function SideBar() {
+    return <h1 className={styles.sidebar }>This is the sidebar.</h1>
+}
+
+export default SideBar;
