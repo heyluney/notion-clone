d1ecadd5fddc03c5b45b35146db3b5650970daa5
helenyu <rinmuses@gmail.com>
2023-08-22 10:14:19 -0700

    Make comments editable. Fix defaultValue warning.

--- src/components/comment/Comments.jsx
+++ src/components/comment/Comments.jsx
@@ -5,6 +5,7 @@ import clark from '../../assets/clark_profile.jpg';
 
 import getTimeString from '../../utils/calculate_time_elapsed';
 
+
 const Comments = () => {
     if (localStorage.getItem('quicknote-comments') == null || localStorage.getItem('quicknote-comments') === 'undefined') {
         localStorage.setItem('quicknote-comments', JSON.stringify([]));
@@ -12,29 +13,65 @@ const Comments = () => {
     const savedComments = JSON.parse(localStorage.getItem('quicknote-comments'));
     const [comments, addComment] = useState(savedComments);
     const [currentComment, updateComment] = useState("");
+
+    
+    const [commentBeingMousedOver, changeMouseOver] = useState(-1);
+    const [commentBeingEdited, changeEdit] = useState(-1);
+
     return (
         <div>
-            This is comment
-            {Object.keys(comments).length == 0 ? "" : comments.map(comment => 
-                <div className={styles.comment} key={comment.timestamp}>
+            {Object.keys(comments).length == 0 ? "" : comments.map((comment, idx) => 
+                <div className={styles.comment} 
+                    key={idx}
+                    onMouseEnter={() => {
+                        changeMouseOver(idx);
+                    }} 
+                    >
+                    
                     <img className={styles.pic} src={clark}/>
                     <div className={styles.text}>
                         <div className={styles.header}>
-                            <div className={styles.author}>Helen Yu</div>
-                            <div className={styles.date}>{getTimeString(comment.timestamp)}</div>
+                            <div className={styles.meta}>
+                                <div className={styles.author}>Helen Yu</div>
+                                <div className={styles.date}>{getTimeString(comment.timestamp)}</div>
+                            </div>
+                            {
+                                idx == commentBeingMousedOver ?
+                                <div>
+                                    <button onClick={() => {
+                                        changeEdit(idx);
+                                    }}>Edit</button>
+                                    <button>Delete</button> 
+                                </div> : ""
+                            }
                         </div>
-                        <div className={styles.textarea}>{comment.comment}</div>
+                        <textarea readOnly={!(idx == commentBeingEdited)} 
+                                    defaultValue={comment.comment}
+                                    className={styles.textarea}
+                                    onKeyDown={(e) => {
+                                        if (e.key === 'Enter') {
+                
+                                            comments[idx] = {timestamp: JSON.stringify(Date.now()), 
+                                                comment: e.target.value};
+                                            addComment(comments);
+                                            localStorage.setItem('quicknote-comments', JSON.stringify(comments));
+                                            changeMouseOver(-1);
+                                            changeEdit(-1);
+                                        }
+                                    }} />
                     </div>
                 </div>
             )}
+
             <textarea 
                 name="postContent" 
                 value={currentComment}
                 onChange={(e) => {
                     updateComment(e.target.value);
+
                 }}
                 onKeyDown={(e) => {
-                    console.log('e.key', e.key);
+                    console.log('hi')
                     if (e.key === 'Enter') {
                         const currentTimestamp = JSON.stringify(new Date());
                         comments.push({timestamp: currentTimestamp, comment: currentComment});
--- src/utils/calculate_time_elapsed.js
+++ src/utils/calculate_time_elapsed.js
@@ -1,14 +1,33 @@
+const num_to_month_mapping = {
+    1: 'Jan',
+    2: 'Feb',
+    3: 'Mar',
+    4: 'Apr',
+    5: 'May',
+    6: 'Jun',
+    7: 'Jul',
+    8: 'Aug',
+    9: 'Sept',
+    10: 'Oct',
+    11: 'Nov',
+    12: 'Dec'
+}
+
 const getTimeString = (stringified_date) => {
-    const current = new Date().getTime();
-    const old = new Date(JSON.parse(stringified_date)).getTime();
-    const minutes = Math.floor((current - old)/(1000*60));
-    if (minutes < 1) {
-        return "Now";
+    const currentDate = new Date();
+    const oldDate = new Date(JSON.parse(stringified_date));
+
+    const minutes = Math.floor((currentDate.getTime() - oldDate.getTime())/(1000*60));
+    const hours = Math.floor(minutes / 60);
+
+    if (hours > 12) {
+        return num_to_month_mapping[oldDate.getMonth()] + oldDate.getDate();
     } else if (minutes > 60) {
-        const hours = Math.floor(minutes / 60);
         return `${hours} hour${hours == 1 ? "": "s"} ago`;
-    } else {
+    } else if (minutes > 1) {
         return `${minutes} minute${minutes == 1 ? "": "s"} ago`;
+    } else {
+        return "Now";
     }
 }
 
