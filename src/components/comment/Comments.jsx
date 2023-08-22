00602421b366a9743ba4a8f704bca3f25f1193d8
helenyu <rinmuses@gmail.com>
2023-08-22 16:42:47 -0700

    Use EffectHook to update background color to stimulate background being darkened when popup comes up.

--- src/components/comment/Comments.jsx
+++ src/components/comment/Comments.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import styles from './Comments.module.css';
 
 import clark from '../../assets/clark_profile.jpg';
@@ -10,6 +10,9 @@ const Comments = () => {
     if (localStorage.getItem('quicknote-comments') == null || localStorage.getItem('quicknote-comments') === 'undefined') {
         localStorage.setItem('quicknote-comments', JSON.stringify([]));
     }
+    const [color, setColor] = useState("white"); 
+    const [opacity, setOpacity] = useState(1);
+
     const savedComments = JSON.parse(localStorage.getItem('quicknote-comments'));
     const [comments, addComment] = useState(savedComments);
     const [currentComment, updateComment] = useState("");
@@ -17,6 +20,11 @@ const Comments = () => {
     
     const [commentBeingMousedOver, changeMouseOver] = useState(-1);
     const [commentBeingEdited, changeEdit] = useState(-1);
+
+    useEffect(() => {
+        document.body.style.backgroundColor = color;
+        document.body.style.opacity = opacity;
+    }, [color, opacity]);
 
     return (
         <div className={styles.comments}>
@@ -42,8 +50,8 @@ const Comments = () => {
                                         changeEdit(idx);
                                     }}>Edit</button>
                                     <button onClick={() => {
-                                        comments.shift();
-                                        addComment(comments);
+                                        setColor('rgba(111, 111, 111, 1)');
+                                        setOpacity(0.5);
                                     }}>Delete</button> 
                                 </div> : ""
                             }
