9f55a694c4df48b29b5a42f0837f302f0a24d944
helenyu <rinmuses@gmail.com>
2023-10-14 11:38:34 -0700

    Add /notion-clone/ as another alt for serving Home component.

--- src/utils/url_to_component_map.js
+++ src/utils/url_to_component_map.js
@@ -2,6 +2,7 @@
 const url_map = {
     "/": "Home",
     "/notion-clone": "Home",
+    "/notion-clone/": "Home",
     "/quick_note": "Quick Note",
     "/task_list": "Task List",
     "/journal": "Journal"
